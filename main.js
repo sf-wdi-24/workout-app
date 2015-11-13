1180ca59be420a12939225b7be11e51b6763bb4f
cameronjacoby <cameron.jacoby@gmail.com>
2015-11-13 11:39:41 -0800

    set up handlebars client-side templating

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 $(function() {
   
+  // compile handlebars template
+  var source = $('#workouts-template').html();
+  var template = Handlebars.compile(source);
 
+  // array of test data
+  var allWorkouts = [
+    { exercise: 'Ran 3 miles', category: 'cardio' },
+    { exercise: 'Yoga', category: 'stretching' }
+  ];
+
+  var workoutsHtml = template({ workouts: allWorkouts });
+  $('#workouts-list').append(workoutsHtml);
 
 });
